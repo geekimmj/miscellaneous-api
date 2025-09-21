1c6be5e7a00232607b6b52621da9b89b5a6a8c0f
geekimmj <geekimmj@gmail.com>
2025-09-21 14:12:48 +0900

    Add: rest api sample
    - add sample code REST API server using shelf.dart

--- bin/miscellaneous_api.dart
+++ bin/miscellaneous_api.dart
@@ -1,1 +1,14 @@
-void main(List<String> arguments) {}
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+
+void main(List<String> args) async {
+  final router = Router();
+
+  router.get("/", (Request req) {
+    return Response.ok("hi");
+  });
+
+  final server = await serve(router, "localhost", 8000);
+}
